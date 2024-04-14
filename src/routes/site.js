5fb9965208de4865bc2d61e89105fcbbadc01c6c
ngochau2004 <vuongngochau2004@gmail.com>
2024-04-14 20:12:04 +0700

    Install prettier lint-staged husky

--- src/routes/site.js
+++ src/routes/site.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const siteController = require('../app/controllers/SiteController');
 
 router.use('/search', siteController.search);
-router.use('/',          siteController.index);
+router.use('/', siteController.index);
 
 module.exports = router;
